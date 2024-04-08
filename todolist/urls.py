9f6c1134ed9c82ea2812db2dabd43a188c16b8d2
Salomov-Omadbek <153747643+Salomov-Omadbek@users.noreply.github.com>
2024-04-08 16:16:00 +0500

    Update urls.py

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", include("lists.urls")),
@@ -8,3 +10,5 @@ urlpatterns = [
     path("api-auth/", include("rest_framework.urls")),
     path("admin/", admin.site.urls),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
